2a7c6b9e071ce8fccf668a0ed5628fef2a309846
Mohammed Jewel <jewelnabin@gmail.com>
2022-05-22 15:27:48 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Purchase from './Pages/Purchase/Purchase';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="purchase" element={<Purchase />} />
       </Routes>
     </div>
   );
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     const menuItems = <>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/appointment">Purchase</Link></li>
+        <li><Link to="/purchase">Purchase</Link></li>
     </>
     return (
         <div className="navbar bg-base-100">
