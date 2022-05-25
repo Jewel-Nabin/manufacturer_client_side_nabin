9f039fe60f57dcca23ae972fe5b580f81f7cf88d
Mohammed Jewel <jewelnabin@gmail.com>
2022-05-25 14:07:04 +0600

    jwtToken Applied

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import auth from '../../firebase.init';
-import MyProfile from '../Dashboard/MyProfile';
 import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfile } from 'react-firebase-hooks/auth';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
+import useToken from './useToken';
 
 const SignUp = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
@@ -21,27 +21,29 @@ const SignUp = () => {
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
 
+    const [token] = useToken(user || gUser);
+
+
     const navigate = useNavigate();
+
+    let signInError;
 
     if (loading || gLoading || updating) {
         return <Loading />
     }
 
-    if (user || gUser) {
-        console.log(user || gUser);
-    }
-
-    let signInError;
-
     if (error || gError || updateError) {
         signInError = <p className='text-red-500'><small>{error?.message || gError?.message || updateError?.message}</small></p>
+    }
+
+    if (token) {
+        navigate('/purchase');
     }
 
     const onSubmit = async data => {
         await createUserWithEmailAndPassword(data.email, data.password);
         await updateProfile({ displayName: data.name });
         console.log('update done');
-        navigate('/purchase');
 
     }
     return (
